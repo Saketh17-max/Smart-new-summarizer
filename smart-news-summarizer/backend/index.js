82dcea06590c248ee7b4d847d486f66c518db473
Saketh <venkatsakethtammineni17@gmail.com>
2026-03-09 16:26:01 +0530

    add root route

--- smart-news-summarizer/backend/index.js
+++ smart-news-summarizer/backend/index.js
@@ -14,7 +14,9 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
-
+app.get("/", (req, res) => {
+  res.send("Smart News Summarizer API Running 🚀");
+});
 app.post('/api/analyze', async (req, res) => {
     const { urls } = req.body;
     if (!urls || !Array.isArray(urls) || urls.length === 0) {
